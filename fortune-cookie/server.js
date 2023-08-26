51d9bf914ec377f71a1f707131fd79bf836fcc30
Ram Budha <devops.rambudha@gmail.com>
2023-08-26 14:13:42 +1000

    Merged

---
+++ fortune-cookie/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const child_process = require('child_process');
+// Create a new Express web application
+const app = express();
+// Set handler for the index of the website
+app.get('/', (req, res) => {
+    // Run the system `fortune` command and respond with the message
+    child_process.exec('fortune', (error, message) => {
+        console.log("eroorer", error, message)
+        if (error === null) {
+            res.send(message);
+        } else {
+            res.send('Error: ' + error);
+        }
+    })
+});
+// Start web application server
+app.listen(3000, () => {
+    console.log('Server started');
+});
